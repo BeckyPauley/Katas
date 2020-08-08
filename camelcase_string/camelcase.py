5c2802cb114b9a203913a8d07cdd4597f68ecbcf
Becky P <64131207+BeckyPauley@users.noreply.github.com>
2020-08-08 19:23:14 +0100

    This commit refines the solution by using a list comprehension

--- camelcase_string/camelcase.py
+++ camelcase_string/camelcase.py
@@ -1,21 +1,12 @@
 def to_camel_case(text):
     capitalised = []
-    for word in text.split(check_splitter(text)):
-        capitalised.append(word.capitalize())
+    [capitalised.append(word.capitalize()) for word in text.split(check_splitter(text))]
     if text[0].islower() == True:
         capitalised[0] = capitalised[0].lower()
-    print(check_splitter(text).join(capitalised))
+    return check_splitter(text).join(capitalised)
 
 def check_splitter(text):
     if '-' in text:
         return '-'
     elif '_' in text:
         return '_'
-
-#split string into words based on - or _ characters
-
-#  if first letter capitalised, return that word with capital
-
-to_camel_case("the-stealth-warrior") # returns "theStealthWarrior"
-
-to_camel_case("The_Stealth_Warrior") # returns "TheStealthWarrior"
