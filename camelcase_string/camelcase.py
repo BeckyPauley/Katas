f9075a1a1b832231800fb43416b928bf4464401a
Becky P <64131207+BeckyPauley@users.noreply.github.com>
2020-08-09 12:12:12 +0100

    This commit solves bug with empty string

--- camelcase_string/camelcase.py
+++ camelcase_string/camelcase.py
@@ -1,12 +1,20 @@
 def to_camel_case(text):
     capitalised = []
-    [capitalised.append(word.capitalize()) for word in text.split(check_splitter(text))]
-    if text[0].islower() == True:
-        capitalised[0] = capitalised[0].lower()
-    return check_splitter(text).join(capitalised)
+    if len(text) >= 1:
+        [capitalised.append(word.capitalize()) for word in text.split(check_splitter(text))]
+        if text[0].islower():
+            capitalised[0] = capitalised[0].lower()
+        return check_splitter(text).join(capitalised)
+    else:
+        return text
 
 def check_splitter(text):
     if '-' in text:
         return '-'
     elif '_' in text:
         return '_'
+
+to_camel_case('')
+to_camel_case("the_stealth_warrior")
+to_camel_case("The-Stealth-Warrior")
+to_camel_case("A-B-C")
