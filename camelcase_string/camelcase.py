09289dc1c0f945d1f44a3d48b444b506bdce2a36
Becky P <64131207+BeckyPauley@users.noreply.github.com>
2020-08-09 12:37:57 +0100

    This commit allows for a combination of hyphens and dashes, rather than only one punctuation type

--- camelcase_string/camelcase.py
+++ camelcase_string/camelcase.py
@@ -1,18 +1,14 @@
 def to_camel_case(text):
     capitalised = []
     if len(text) >= 1:
-        [capitalised.append(word.capitalize()) for word in text.split(check_splitter(text))]
+        replaced_string = text.replace('_', ' ').replace('-', ' ')
+        [capitalised.append(word.capitalize()) for word in replaced_string.split(' ')]
         if text[0].islower():
             capitalised[0] = capitalised[0].lower()
         return ''.join(capitalised)
     else:
         return text
 
-def check_splitter(text):
-    if '-' in text:
-        return '-'
-    elif '_' in text:
-        return '_'
 
 to_camel_case('')
 to_camel_case("the_stealth_warrior")
